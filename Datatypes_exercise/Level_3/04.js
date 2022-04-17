f868d358f1126b88ce82eefd6d6fd767512a804c
Philip K. Oyelegbin <72349309+PhilipOyelegbin@users.noreply.github.com>
2022-04-17 21:21:19 +0100

    Add files via upload

--- Datatypes_exercise/Level_3/04.js
+++ Datatypes_exercise/Level_3/04.js
@@ -1,4 +1,3 @@
 let text = "He earns 5000 euros from salary per month, 10000 euro annual bonus, 15000 euro online course per month."
-let income = Number(text[])
-let expenditure = +text[]
+let total = (Number(text.match("5000")) *12) + (+text.match("10000")) + ((+text.match("15000")) * 12)
 console.log(total)
