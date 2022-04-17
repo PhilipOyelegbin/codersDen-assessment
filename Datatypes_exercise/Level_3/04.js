345a1ee136e90e35fdfc036f4ced1c626c3184dc
Philip K. Oyelegbin <72349309+PhilipOyelegbin@users.noreply.github.com>
2022-04-17 20:38:58 +0100

    Update 04.js

--- Datatypes_exercise/Level_3/04.js
+++ Datatypes_exercise/Level_3/04.js
@@ -1,3 +1,4 @@
-let num = "9.8"
-console.log(parseFloat(num))
-console.log(Math.round(num))
+let text = "He earns 5000 euros from salary per month, 10000 euro annual bonus, 15000 euro online course per month."
+let income = Number(text[])
+let expenditure = +text[]
+console.log(total)
